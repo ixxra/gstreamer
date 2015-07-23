60f64ec344d94a9f1b7e77d99573cfa654c436fd
Rene Garcia <garciamx@gmail.com>
2015-07-23 11:40:22 -0500

    works

--- app.py
+++ app.py
@@ -1,69 +1,9 @@
 from gi.repository import Gtk
 from gi.repository import Gst
-from os.path import basename
 from playlist import Playlist
 from player import Player
 from controllers.timeBar import TimeBarCtrl
-
-COLS = {
-        'title': 0,
-        'artist': 1,
-        'album': 2,
-        'location': 3}
-
-
-class Controller:
-    def __init__(self, playlist):
-        self.mediaPlayer = Player()
-        self.playlist = playlist
-
-    def playSelected(self, tree, path, column):
-        model = tree.get_model()
-        it = model.get_iter(path)
-        self.playlist.select(it)
-        location = model.get_value(it, COLS['location'])
-        self.mediaPlayer.set_mrl(location)
-        self.mediaPlayer.play()
-
-    def play(self, *args):
-        self.mediaPlayer.play()
-
-    def pause(self, *args):
-        self.mediaPlayer.pause()
-
-    @staticmethod
-    def addFiles(store):
-        d = Gtk.FileChooserDialog(
-            'Add media',
-            None,
-            Gtk.FileChooserAction.OPEN)
-        d.add_button('Accept', Gtk.ResponseType.OK)
-        d.add_button('Cancel', Gtk.ResponseType.CANCEL)
-        d.set_select_multiple(True)
-        res = d.run()
-
-        if res == Gtk.ResponseType.OK:
-            for f in d.get_filenames():
-                it = store.append()
-                store.set_value(it, COLS['title'], basename(f))
-                store.set_value(it, COLS['location'], f)
-
-        d.destroy()
-
-    def removeFromPlaylist(self, selection):
-        model, paths = selection.get_selected_rows()
-        refs = [Gtk.TreeRowReference.new(model, p) for p in paths]
-        for ref in refs:
-            it = model.get_iter(ref.get_path())
-            model.remove(it)
-
-    @staticmethod
-    def clearPlaylist(store):
-        store.clear()
-
-    @staticmethod
-    def quit(*args):
-        Gtk.main_quit()
+from controllers.main import MainCtrl
 
 
 class Application:
@@ -71,7 +11,7 @@ class Application:
         b = Gtk.Builder.new_from_file('app.glade')
         store = b.get_object('liststore1')
         playlist = Playlist(store)
-        ctrl = Controller(playlist)
+        ctrl = MainCtrl(playlist)
         b.connect_signals(ctrl)
         durationLabel = b.get_object('totalDuration')
         positionScale = b.get_object('position')
