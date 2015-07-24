9862be1c043bb3dc24ba440ec30d610791744a8b
Rene Garcia <garciamx@gmail.com>
2015-07-23 23:28:34 -0500

    title-artist-album-genre

--- widgets/library_search.py
+++ widgets/library_search.py
@@ -10,6 +10,14 @@ COL = {
     'fileName' : 2,
     'fileLocation': 3}
 
+HEADERS = ('title', 'artist', 'album', 'genre')
+
+NEW_COLS = {
+    'title': 0,
+    'artist': 1,
+    'album': 2,
+    'genre': 3,
+    'fileLocation': 4}
 
 class Main(Gtk.ApplicationWindow):
     def __init__(self):
@@ -32,7 +40,8 @@ class Main(Gtk.ApplicationWindow):
         scroll = Gtk.ScrolledWindow()
         scroll.add(view)
         self.do_make_view_columns(view)
-        store = Gtk.ListStore(str, str, str, str)
+        store = Gtk.ListStore(str, str, str, str, str)
+        #store = Gtk.TreeStore(str, str, str, str)
         view.set_model(store)
         box = Gtk.VBox(False, 0)
         box.pack_start(hbox, False, True, 5)
@@ -49,22 +58,28 @@ class Main(Gtk.ApplicationWindow):
         q = Tag.select(Tag.file).where(Tag.value.contains(text)).distinct()
         q2 = File.select(File,Tag).join(Tag).where(Tag.file << q).aggregate_rows()
         for file in q2:
-            it = store.append()
+            it = store.append(None)
             uri = file.uri
-            store.set_value(it, COL['fileName'], basename(uri))
-            store.set_value(it, COL['fileLocation'], uri)
+            store.set_value(it, NEW_COLS['title'], basename(uri))
+            store.set_value(it, NEW_COLS['fileLocation'], uri)
+            count = len(HEADERS)
             for tag in file.tags:
-                it = store.append()
-                store.set_value(it, COL['key'], tag.key)
-                store.set_value(it, COL['value'], tag.value)
+                if tag.key in HEADERS:
+                    store.set_value(it, NEW_COLS[tag.key], tag.value)
+                    count -= 1
+                if count == 0:
+                    break
+                #child = store.append(it)
+                #store.set_value(child, COL['key'], tag.key)
+                #store.set_value(child, COL['value'], tag.value)
 
     def do_make_view_columns(self, view):
         render = Gtk.CellRendererText()
-        for t in 'key', 'value', 'fileName':
+        for h in HEADERS:
             col = Gtk.TreeViewColumn()
-            col.set_title(t.upper())
+            col.set_title(h.upper())
             col.pack_start(render, True)
-            col.add_attribute(render, 'text', COL[t])
+            col.add_attribute(render, 'text', NEW_COLS[h])
             col.set_expand(True)
             view.append_column(col)
 
